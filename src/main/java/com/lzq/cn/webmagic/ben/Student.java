94fd13ddb84dd84a0e1b764f98cb1b3d158d5483
lg <123456>
2020-07-19 19:49:53 +0800

    git尝试

--- src/main/java/com/lzq/cn/webmagic/ben/Student.java
+++ src/main/java/com/lzq/cn/webmagic/ben/Student.java
@@ -1,4 +1,4 @@
-
+package com.lzq.cn.webmagic.ben;
 
 public class Student{
      public static String name = "lg";
---
+++ src/main/java/com/lzq/cn/webmagic/ben/Teacher.java
@@ -0,0 +1,5 @@
+package com.lzq.cn.webmagic.ben;
+
+public class Teacher {
+    public static String name="zyj";
+}
